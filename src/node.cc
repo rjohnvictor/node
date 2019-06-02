2f015b3fb510838f872fb757e2a03cc7e0c46016
Michaël Zasso <targos@protonmail.com>
2019-06-02 14:30:36 +0200

    src: use non-deprecated overload of V8::SetFlagsFromString

--- src/node.cc
+++ src/node.cc
@@ -946,7 +946,8 @@ void Init(int* argc,
   }
 
   if (per_process::cli_options->print_v8_help) {
-    V8::SetFlagsFromString("--help", 6);  // Doesn't return.
+    // Doesn't return.
+    V8::SetFlagsFromString("--help", static_cast<size_t>(6));
     UNREACHABLE();
   }
 
@@ -1004,7 +1005,8 @@ InitializationResult InitializeOncePerProcess(int argc, char** argv) {
   }
 
   if (per_process::cli_options->print_v8_help) {
-    V8::SetFlagsFromString("--help", 6);  // Doesn't return.
+    // Doesn't return.
+    V8::SetFlagsFromString("--help", static_cast<size_t>(6));
     UNREACHABLE();
   }
 
